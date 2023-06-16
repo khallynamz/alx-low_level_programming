1304e6cdf1e85aee3d7bca87de30241d7f22ff26
khallynamz <kalimattijjani@gmail.com>
2023-06-16 05:15:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - "A pragram that prints a new line with prinf," followed newline
- * Return: 0.
+ * main - A pragram that prints a new line with prinf followed newline
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammer,but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammer,but the outcome is a piece of art,\n");
+return (0);
 }
